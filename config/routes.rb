e2549e6a95b6c676bf0b3d09cace39d58ef4b960
Aseiide <eiseiide@gmail.com>
2020-12-11 02:35:26 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'cafes/index'
-  get 'cafes/show'
+  root 'cafes#index'
+  resources :cafes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
